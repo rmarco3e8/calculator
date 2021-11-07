6be47878261e499e3763e1da0d832ff6d529e2f9
Marco Rodrigues <mrodrigues3e8@gmail.com>
2021-11-07 10:43:29 -0500

    Add css, custom font, and basic functionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+content = "0";
+num1 = NaN;
+num2 = NaN;
+op = "";
+
+
 function add(a, b) {
     return a + b;
 }
@@ -11,18 +17,83 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    return Math.floor(a / b);
 }
 
 function operate(operator, a, b) {
     switch (operator) {
-        case "add":
+        case "+":
             return add(a, b);
-        case "subtract":
+        case "-":
             return subtract(a, b);
-        case "multiply":
+        case "x":
             return multiply(a, b);
-        case "add":
+        case "÷":
             return divide(a, b);
+        default:
+            return;
     }
 }
+
+function numButton(e) {
+    input = e.target.innerText;
+    updateContent(input);
+}
+
+function updateContent(input) {
+    if (!isNaN(input)) {
+        if (content === "0") {
+            content = "";
+        }
+        content += input;
+        changeDisplay(content);
+        return;
+    } 
+    
+    else if (input === ".") {
+        console.log("TODO");
+        return;
+    } 
+    
+    else if (input === "=") {
+        num2 = parseInt(content);
+        res = operate(op, num1, num2);
+        num1 = NaN;
+        num2 = NaN;
+        content = toString(res);
+        changeDisplay(res);
+    }
+     
+    else {
+        num1 = parseInt(content);
+        op = input;
+        clear();
+    }
+}
+
+function del() {
+    content = content.substring(0, content.length-1);
+    if (content === "") {
+        content = "0";
+    }
+    changeDisplay(content);
+}
+
+function clear() {
+    content = "0";
+    changeDisplay(content);
+}
+
+function changeDisplay(content) {
+    display = document.querySelector(".display");
+    display.innerText=content;
+}
+
+padButtons = document.querySelectorAll(".pad");
+padButtons.forEach(padButton => padButton.addEventListener('click', numButton));
+
+clearButton = document.querySelector(".clear");
+clearButton.addEventListener('click', clear);
+
+delButton = document.querySelector(".delete");
+delButton.addEventListener('click', del);
